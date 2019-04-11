35eafb5f0c10cd6609aaa5b3ee3da32a781ce3b2
Minh Duong <minhduongnhat@outlook.com>
2019-04-11 21:59:04 +0700

    implement bootstrap framework into the project

---
+++ Code/NewsManagement/Controllers/SpeakersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsManagement.Controllers
+{
+    public class SpeakersController : Controller
+    {
+        // GET: Speakers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
